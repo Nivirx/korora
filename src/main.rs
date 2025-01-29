9b3718d2ae380583acfcdff5b271d34b4a341275
Elaina Claus <elaina.claus@chtm.me>
2025-01-29 14:38:34 -0500

    added blinky example from rp235x examples as a sanity check so far

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 #![no_std]
 #![no_main]
 use defmt::*;
+
+use rp235x_hal as hal;
 use rp235x_hal::block::ImageDef;
 use rp235x_hal::{binary_info, entry};
+
+use embedded_hal::delay::DelayNs;
+use embedded_hal::digital::OutputPin;
 
 use defmt_rtt as _;
 use panic_halt as _;
@@ -28,6 +33,9 @@ const WIFI_FIRMWARE_BASE: u32 = 0x1038_0000;
 const BT_FIRMWARE_BASE: u32 = 0x103C_0000;
 const CLM_FIRMWARE_BASE: u32 = 0x103C_4000;
 
+/// External high-speed crystal on the Raspberry Pi Pico 2 board is 12 MHz.
+const XTAL_FREQ_HZ: u32 = 12_000_000u32;
+
 #[entry]
 fn main() -> ! {
     let _fw = unsafe { core::slice::from_raw_parts(WIFI_FIRMWARE_BASE as *const u8, 231077) };
@@ -35,7 +43,44 @@ fn main() -> ! {
     let _clm = unsafe { core::slice::from_raw_parts(CLM_FIRMWARE_BASE as *const u8, 984) };
 
     info!("A Rusty 'Hello!' from RISC-V land!");
+
+    // Grab our singleton objects
+    let mut pac = hal::pac::Peripherals::take().unwrap();
+
+    // Set up the watchdog driver - needed by the clock setup code
+    let mut watchdog = hal::Watchdog::new(pac.WATCHDOG);
+
+    // Configure the clocks
+    let clocks = hal::clocks::init_clocks_and_plls(
+        XTAL_FREQ_HZ,
+        pac.XOSC,
+        pac.CLOCKS,
+        pac.PLL_SYS,
+        pac.PLL_USB,
+        &mut pac.RESETS,
+        &mut watchdog,
+    )
+    .unwrap();
+
+    let mut timer = hal::Timer::new_timer0(pac.TIMER0, &mut pac.RESETS, &clocks);
+
+    // The single-cycle I/O block controls our GPIO pins
+    let sio = hal::Sio::new(pac.SIO);
+
+    // Set the pins to their default state
+    let pins = hal::gpio::Pins::new(
+        pac.IO_BANK0,
+        pac.PADS_BANK0,
+        sio.gpio_bank0,
+        &mut pac.RESETS,
+    );
+
+    // Configure GPIO15 as an output
+    let mut led_pin = pins.gpio15.into_push_pull_output();
     loop {
-        riscv::asm::nop();
+        led_pin.set_high().unwrap();
+        timer.delay_ms(500);
+        led_pin.set_low().unwrap();
+        timer.delay_ms(500);
     }
 }
